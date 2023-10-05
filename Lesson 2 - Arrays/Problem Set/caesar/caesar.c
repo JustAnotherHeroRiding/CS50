4a38dafb9f2e1e76127d3e22a7f3b6ac96b6a321
Kristijan Kocev <kristijankocev1234@gmail.com>
2023-10-05 21:02:59 +0200

    validating key

--- Lesson 2 - Arrays/Problem Set/caesar/caesar.c
+++ Lesson 2 - Arrays/Problem Set/caesar/caesar.c
@@ -1,15 +1,42 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+
+bool isNonNegativeInteger(string str);
 
 int main(int argc, string argv[])
 {
-    if (argc != 2 || (atoi(argv[1]) < 0))
+    if (argc != 2 || !isNonNegativeInteger(argv[1]))
     {
         printf("Usage: ./caesar key\n");
         return 1;
     }
-    
-    return 0;
 
+    // If you reach this point, argv[1] is a valid non-negative integer
+    int key = atoi(argv[1]);
+    key = key % 26;
+    // Your program logic here
+    //printf("%i\n", key);
+
+
+    string plaintext = get_string("plaintext: ");
+    string ciphertext;
+    printf("ciphertext: %s", ciphertext);
+    printf("\n");
+    return 0;
+}
+
+bool isNonNegativeInteger(string str)
+{
+    for (int i = 0; str[i] != '\0'; i++)
+    {
+        if (!isdigit(str[i]))
+        {
+            return false; // Not a digit
+        }
+    }
+
+    int num = atoi(str);
+    return num >= 0; // Check if it's non-negative
 }
