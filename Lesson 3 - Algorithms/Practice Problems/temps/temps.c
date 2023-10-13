9ab4adf14dcb7016ba5b3bdb1442aa270b9947b9
Kristijan Kocev <kristijankocev1234@gmail.com>
2023-10-13 20:04:08 +0200

    added pseudo code for selection sort

--- Lesson 3 - Algorithms/Practice Problems/temps/temps.c
+++ Lesson 3 - Algorithms/Practice Problems/temps/temps.c
@@ -60,6 +60,17 @@ int main(void)
 }
 
 // TODO: Sort cities by temperature in descending order
+/* repeat (numOfElements - 1) times
+
+  set the first unsorted element as the minimum
+
+  for each of the unsorted elements
+
+    if element < currentMinimum
+
+      set element as new minimum
+
+  swap minimum with first unsorted position */
 void sort_cities(void)
 {
     // Add your code here
