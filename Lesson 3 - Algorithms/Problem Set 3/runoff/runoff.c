f444837da396c5c050982a64be9ba1a213cfb9dc
Kristijan Kocev <kristijankocev1234@gmail.com>
2023-10-14 20:15:30 +0200

    solved runoff

--- Lesson 3 - Algorithms/Problem Set 3/runoff/runoff.c
+++ Lesson 3 - Algorithms/Problem Set 3/runoff/runoff.c
@@ -192,7 +192,7 @@ void sort_winners(int length)
 bool print_winner(void)
 {
     float requiredVotes = ceil((float)voter_count / 2);
-    //printf("Required votes: %i\n", (int)requiredVotes);
+    // printf("Required votes: %i\n", (int)requiredVotes);
     for (int i = 0; i < candidate_count; i++)
     {
         // printf("%s: %i\n", candidates[i].name, candidates[i].votes);
@@ -210,20 +210,42 @@ bool print_winner(void)
 // Return the minimum number of votes any remaining candidate has
 int find_min(void)
 {
+    int min = candidates[0].votes;
     // TODO
-    return 0;
+    for (int i = 1; i < candidate_count; i++)
+    {
+        if (candidates[i].votes < min && candidates[i].eliminated == false)
+        {
+            min = candidates[i].votes;
+        }
+    }
+    return min;
 }
 
 // Return true if the election is tied between all candidates, false otherwise
 bool is_tie(int min)
 {
     // TODO
-    return false;
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (candidates[i].votes != min && candidates[i].eliminated == false)
+        {
+            return false;
+        }
+    }
+    return true;
 }
 
 // Eliminate the candidate (or candidates) in last place
 void eliminate(int min)
 {
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (candidates[i].votes == min && candidates[i].eliminated == false)
+        {
+            candidates[i].eliminated = true;
+        }
+    }
     // TODO
     return;
 }
