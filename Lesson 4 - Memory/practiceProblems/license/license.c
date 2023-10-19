fd34de1849d8d5824bdde0544730d50d27d4896a
Kristijan Kocev <kristijankocev1234@gmail.com>
2023-10-19 17:41:01 +0200

    finished license plates

--- Lesson 4 - Memory/practiceProblems/license/license.c
+++ Lesson 4 - Memory/practiceProblems/license/license.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[])
 {
@@ -12,8 +14,10 @@ int main(int argc, char *argv[])
     // Create buffer to read into
     char buffer[7];
 
+    // printf("%s\n", buffer);
+
     // Create array to store plate numbers
-    char *plates[8];
+    char plates[8][8];
 
     FILE *infile = fopen(argv[1], "r");
 
@@ -23,16 +27,27 @@ int main(int argc, char *argv[])
     {
         // Replace '\n' with '\0'
         buffer[6] = '\0';
-
         // Save plate number in array
-        plates[idx] = buffer;
-        printf("Plate number: %s, index = %i, saved plate number = %s\n", buffer, idx, plates[idx]);
+        // Copy the plate number into the array
+        strcpy(plates[idx], buffer);
+        //plates[idx] = strdup(buffer);
+        // printf("Plate number: %s, index = %i, saved plate number = %s\n", buffer, idx, plates[idx]);
 
+        // The pointers for buffer and plates[idx] are the same
+        // They return the same memory block
+        // This means that the same memory block keeps getting changed
+        // The reason that plates array prints the same is that all the elements
+        // in the array just a pointer to the same address
         idx++;
     }
 
-    /* for (int i = 0; i < 8; i++)
+    // printf("Buffer pointer %p plates[5] pointer %p\n", buffer, plates[5]);
+    for (int i = 0; i < 8; i++)
     {
         printf("%s\n", plates[i]);
+    }
+   /*  for (int i = 0; i < idx; i++)
+    {
+        free(plates[i]);
     } */
 }
