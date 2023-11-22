90c69bb2b69527f87fa10ad6c6eb0bd8323a3e41
Kristijan Kocev <kristijankocev1234@gmail.com>
2023-11-22 17:57:40 +0100

    cleaned up roster.py

--- Lesson 7 - SQL/PracticeProblems/prophecy/roster.py
+++ Lesson 7 - SQL/PracticeProblems/prophecy/roster.py
@@ -4,19 +4,18 @@ import csv
 db = SQL("sqlite:///roster.db")
 
 # Step 1: Create tables as per your schema
-# db.execute("CREATE TABLE IF NOT EXISTS students (id INT, student_name TEXT, PRIMARY KEY(id))")
-# db.execute("CREATE TABLE IF NOT EXISTS houses (id INT, house TEXT, head TEXT, PRIMARY KEY(id))")
-# db.execute("CREATE TABLE IF NOT EXISTS studentHouse (id INT, student_id INT, house_id INT, PRIMARY KEY(id), FOREIGN KEY(student_id) REFERENCES students(id), FOREIGN KEY(house_id) REFERENCES houses(id))")
+db.execute("CREATE TABLE IF NOT EXISTS students (id INTEGER PRIMARY KEY AUTOINCREMENT, student_name TEXT)")
+db.execute("CREATE TABLE IF NOT EXISTS houses (id INTEGER PRIMARY KEY AUTOINCREMENT, house TEXT, head TEXT)")
+db.execute("CREATE TABLE IF NOT EXISTS studentHouse (id INTEGER PRIMARY KEY AUTOINCREMENT, student_id INT, house_id INT, FOREIGN KEY(student_id) REFERENCES students(id), FOREIGN KEY(house_id) REFERENCES houses(id))")
 
 # Step 2: Read and process CSV data
-""" with open("students.csv", "r") as file:
+with open("students.csv", "r") as file:
     reader = csv.DictReader(file)
 
     for row in reader:
         # Insert into students
         db.execute(
-            "INSERT INTO students (id, student_name) VALUES (:id, :name)",
-            id=row["id"],
+            "INSERT INTO students (student_name) VALUES ( :name)",
             name=row["student_name"],
         )
 
@@ -29,7 +28,7 @@ db = SQL("sqlite:///roster.db")
                 "INSERT INTO houses (house, head) VALUES (:house, :head)",
                 house=row["house"],
                 head=row["head"],
-            ) """
+            )
 
 # After populating students and houses, populate studentHouse
 """ db.execute("CREATE TABLE IF NOT EXISTS new_houses (id INTEGER PRIMARY KEY AUTOINCREMENT, house TEXT, head TEXT)")
@@ -39,11 +38,9 @@ for house in houses:
 db.execute("DROP TABLE houses")
 db.execute("ALTER TABLE new_houses RENAME TO houses") """
 
-students = db.execute("SELECT * FROM students")
-houses = db.execute("SELECT * FROM houses")
 
 # Reading from the CSV and populating studentHouse
-""" with open("students.csv", "r") as file:
+with open("students.csv", "r") as file:
     reader = csv.DictReader(file)
 
     for row in reader:
@@ -63,7 +60,9 @@ houses = db.execute("SELECT * FROM houses")
             "INSERT INTO studentHouse (student_id, house_id) VALUES (:student_id, :house_id)",
             student_id=student_id,
             house_id=house_id,
-        ) """
+        )
 
 studentHouse = db.execute("SELECT * FROM studentHouse")
+students = db.execute("SELECT * FROM students")
+houses = db.execute("SELECT * FROM houses")
 print(studentHouse)
